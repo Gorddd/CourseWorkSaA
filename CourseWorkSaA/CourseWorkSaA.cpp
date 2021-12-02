d7480770cbd025ed1059ed646646b642724381e7
Gorddd <kasual2014@icloud.com>
2021-12-02 16:53:12 +0300

    добавлена меню последовательности

--- CourseWorkSaA/CourseWorkSaA.cpp
+++ CourseWorkSaA/CourseWorkSaA.cpp
@@ -1,7 +1,119 @@
 ﻿#include <stdio.h>
+#include <iostream>
+#include <Windows.h>
+#include <stdlib.h>
+#include "Sequence.h"
 
 /*Множество последовательностей типа float*/ /*L1 список*/
 
-int main() {
+struct SetNode { //Узел множества
+	SequenceNode* data;
+	SetNode* next;
+};
 
+void GetStartedSet(bool& isStarted) {
+	if (isStarted) {
+		system("cls");
+		printf("Вы уже работаете со множеством!\nЧтобы начать работу с новым множеством, закончите с текущим [2]!\n");
+		system("pause");
+	}
+	else
+		isStarted = true;
+}
+
+void FinishWorkSet(bool& isStarted) {
+	isStarted = false;
+
+	system("cls");
+	printf("Вызвана функция чистки множества\n");
+	printf("Вы закончили работу со структурой\n");
+	system("pause");
+}
+
+void isAllocated(SetNode* node) {
+	if (!node) {
+		printf("Память не выделена, программа закроется!");
+		Sleep(1000);
+		exit(0);
+	}
+}
+
+SetNode* InitializeElement() {
+	SetNode* newNode = (SetNode*)malloc(sizeof(SetNode));
+	isAllocated(newNode);
+
+	//Переход в меню последовательности
+	newNode->next = NULL;
+
+	return newNode;
+}
+
+void AddtoSet(SetNode*& head) {
+	SetNode* t = head;
+	
+	while (t) { //Получить последний элемент 
+		t = t->next;
+	}
+
+	t = InitializeElement();
+}
+
+void GetCommandofSet(SetNode*& head, bool& isStarted) {
+	printf("Команда ~ ");
+
+	int command = 0;
+	std::cin >> command;
+
+	switch (command)
+	{
+	case 1:
+		GetStartedSet(isStarted);
+		break;
+	case 2:
+		FinishWorkSet(isStarted);
+		break;
+	case 7:
+
+		break;
+	default:
+		break;
+	}
+}
+
+void PrintSetMenu() {
+	printf("[1] - Начать работу со множеством\n");
+	printf("[2] - Закончить работу со множеством\n");
+	printf("[3] - Сделать пустой\n");
+	printf("[4] - Структура пустая?\n");
+	printf("[5] - Удалить элемент из множества\n");
+	printf("[6] - Взять элемент из множества\n");
+	printf("[7] - Добавить элемент в множество\n");
+	printf("[8] - Проверить принадлежит ли элемент множеству\n");
+	printf("[9] - Распечатать структуру\n");
+	printf("[10] - Завершить работу программы\n");
+}
+
+void SetMenu() {
+	SetNode* head = NULL;
+	bool isStarted = false;
+
+	while (true) {
+		if (head) {
+
+		}
+		else
+			if (isStarted)
+				printf("Структура пустая\n\n");
+
+		PrintSetMenu();
+
+		
+		system("cls");
+	}
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	SetMenu();
 }
--- CourseWorkSaA/Sequence.h
+++ CourseWorkSaA/Sequence.h
@@ -1,1 +1,12 @@
 #pragma once
+
+#include <stdio.h>
+#include <iostream>
+#include <Windows.h>
+#include <stdlib.h>
+
+struct SequenceNode { //Узел последовательности
+	float data;
+	SequenceNode* next;
+};
+
